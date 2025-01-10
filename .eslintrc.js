df9b4e4744fbdd2cdefa1e89f62747db4472ce47
hyejin0662 <hyejin0776@gmail.com>
2025-01-10 15:29:38 +0900

    feat. prettier 설정 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,21 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
+  'prettier/prettier': [
+    'error',
+    {
+      arrowSpacing: ['error', { before: true, after: true }],
+      singleQuote: true,
+      semi: false,
+      useTabs: false,
+      tabWidth: 2,
+      trailingComma: 'none',
+      printWidth: 80,
+      bracketSpacing: true,
+      arrowParens: 'always',
+      endOfLine: 'auto'   // 이 부분이 lf로 되어있다면 auto로 변경
+    }
+  ],
   root: true,
   env: {
     node: true,
