e454d2806d9601f72dc5e59cb166f4ba6859d3ed
hyejin0662 <hyejin0776@gmail.com>
2025-01-10 18:47:06 +0900

    feat. 그래프ql 및 기타 서비스 구현

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -6,7 +6,22 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: [
+        { emit: 'event', level: 'query' },
+        { emit: 'stdout', level: 'info' },
+        { emit: 'stdout', level: 'warn' },
+        { emit: 'stdout', level: 'error' },
+      ],
+      errorFormat: 'colorless',
+    });
+  }
   async onModuleInit() {
+    // this.$on('query', (event: Prisma.QueryEvent) => {
+    //   console.log('Query: ' + event.query);
+    //   console.log('Duration: ' + event.duration + 'ms');
+    // });
     await this.$connect();
   }
 
